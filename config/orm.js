643929d3acd796d4ca26c65a072f660843230118
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-08 23:38:52 -0800

    Added updateOne, insertOne, objToSql, & printQuestionMarks functions.

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,32 @@
 const { query } = require("./connection.js");
 const connection = require("./connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    const arr = [];
+
+    for (let key in ob) {
+      const value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+
+    return arr.toString();
+}
+
 const orm = {
     selectAll: function(tableInput, cb) {
         const queryString = "SELECT * FROM " + tableInput + ";";
@@ -17,7 +43,38 @@ const orm = {
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
-        queryString +=
-    }
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-}
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res);
+        });
+    }
+};
+
+module.exports = orm;
