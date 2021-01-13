c66fcd58ae8b63febe2e1b297003eaa3677fc8c0
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-12 20:09:22 -0800

    Added app.listen and routes.

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const exphbs = require("express-handlebars");
 const express = require("express");
-
+const routes = require("./controllers/burgers_controller");
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(routes);
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
 
