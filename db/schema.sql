4e8f57631e9dee82a83721b3c2b19f57dc9b21d0
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-15 00:49:04 -0800

    Added NOT NULL to devoured column.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR (30) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN NOT NULL,
   PRIMARY KEY(id)
 );
 
