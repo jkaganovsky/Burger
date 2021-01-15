6dcc769ebf60783527d61985c05bbf42e4430f40
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-14 21:38:35 -0800

    Reconfigured syntax for orm create and update to follow jquery.

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,18 @@
 const connection = require("./connection");
 
-function objToSql(ob) {
+function printQuestionMarks(num) {
     const arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+
+function objToSql(ob) {
+    let arr = [];
 
     for (let key in ob) {
       const value = ob[key];
@@ -17,51 +28,41 @@ function objToSql(ob) {
 }
 
 const orm = {
-    selectAll: function(tableInput, cb) {
-        const queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, res) {
+    selectAll: (tableInput, cb) => {
+        const queryString = `SELECT * FROM ${tableInput};`
+        connection.query(queryString, (err, data) => {
                 if (err) {
                     throw err;
                 }
-                cb(res);
+                cb(data);
             });
     },
-    insertOne: function(table, cols, vals, cb) {
-        let queryString = "INSERT INTO " + table;
 
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += "?,?";
-        queryString += ") ";
+    insertOne: (table, cols, vals, cb) => {
+        const queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals)})`
 
         console.log(queryString);
 
-        connection.query(queryString, vals, function(err, res) {
+        connection.query(queryString, vals, (err, data) => {
             if (err) {
                 throw err;
             }
 
-            cb(res);
+            cb(data);
         });
     },
-    updateOne: function(table, objColVals, condition, cb) {
-        const queryString = "UPDATE " + table;
 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+    updateOne: (table, objColVals, condition, cb) => {
+        const queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition};`
 
         console.log(queryString);
 
-        connection.query(queryString, function(err, res) {
+        connection.query(queryString, (err, data) => {
             if (err) {
                 throw err;
             }
 
-            cb(res);
+            cb(data);
         });
     }
 };
