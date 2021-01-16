b4c15c1f733abb5d77b32762d00ba0814395d919
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-16 00:27:06 -0800

    Changed BOOLEAN to default as false.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR (30) NOT NULL,
-  devoured BOOLEAN NOT NULL,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY(id)
 );
 
