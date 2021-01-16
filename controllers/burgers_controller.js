5a772d74f7078a19499380228dc59fd5b9b9256a
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-16 00:26:39 -0800

    Changed res.insertId line 19 to data.insertId.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
     burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(data) {
-        res.json({id: result.insertId});
+        res.json({id: data.insertId});
     });
 });
 
